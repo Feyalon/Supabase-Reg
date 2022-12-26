efa2a3030c8194fdc2795c7126640b48c784d4c8
Feyalon <63968483+Feyalon@users.noreply.github.com>
2022-12-26 18:16:20 +0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://envteevjsxizopfeqedb.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVudnRlZXZqc3hpem9wZmVxZWRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzEzNzg3OTAsImV4cCI6MTk4Njk1NDc5MH0.RgcCPkWH5GDeautOuVpKEnvViVNVreq7fdQi5wSswLQ',
+  supabaseUrl: 'Your Url',
+  supabaseKey: 'Your Key',
   
 }
